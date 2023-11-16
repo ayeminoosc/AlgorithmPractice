34501d8b9bbaa514753a216633de6e41a7433859
Aye Min Oo <ayeminoosc@gmail.com>
2023-11-16 18:48:06 +0800

    p456 not done

---
+++ src/main/java/com/ayeminoo/algorithmpractice/leetcode/medium/P456.java
@@ -0,0 +1,31 @@
+package com.ayeminoo.algorithmpractice.leetcode.medium;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * https://leetcode.com/problems/132-pattern/description/
+ */
+public class P456 {
+//    public boolean find132pattern(int[] nums) {
+//       Map<Integer, Integer> sortedMap = new TreeMap<>();
+//       for(int i = 0; i<nums.length;i++){
+//           sortedMap.put(nums[i], i);
+//       }
+//
+//       int i = 0;
+//       Integer previousIndex = -1;
+//       for(Map.Entry<Integer, Integer> entry: sortedMap.entrySet()){
+//           if(i != 0 && previousIndex > entry.getValue()) return true;
+//           previousIndex = entry.getValue();
+//           i++;
+//       }
+//       return false;
+//    }
+
+    public static void main(String[]args){
+        System.out.println(new P456().find132pattern(new int[]{1,0,1,-4,-3})); // false
+        System.out.println(new P456().find132pattern(new int[]{3,5,  0 ,3,4})); // true
+
+    }
+}
